05271af27319defdc4aad0ad9dbe942fed4bdad0
acecic82 <102938al@naver.com>
2021-09-23 03:17:15 +0900

    인증을 위한 jwt에서 필요한 name, password

---
+++ src/main/java/Auction/service/dto/JwtRequest.java
@@ -0,0 +1,14 @@
+package Auction.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtRequest {
+
+    private String username;
+    private String password;
+}
