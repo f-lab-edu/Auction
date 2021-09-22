2bef3573b49616523315fd46e5ba95f582691c22
acecic82 <102938al@naver.com>
2021-09-23 03:18:54 +0900

    서버가 올라오기전 사용자가 있어야 해당 사용자로 jwt 토큰 테스트가 가능하므로 임시 사용자 디비에 생성하기위한 파일 추가

---
+++ src/main/java/Auction/service/InitDB.java
@@ -0,0 +1,34 @@
+package Auction.service;
+
+import Auction.service.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+
+@Component
+@RequiredArgsConstructor
+public class InitDB {
+
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.dbInit();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+        private final EntityManager em;
+
+        public void dbInit() {
+
+            Member member = new Member("test", "test1234");
+            em.persist(member);
+        }
+    }
+}
