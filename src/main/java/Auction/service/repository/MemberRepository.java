8985cff7c9a53be71afbf8aaa6f91aab902ddeb0
acecic82 <102938al@naver.com>
2021-09-23 03:26:23 +0900

    인증 확인을 위해 userId를 기반으로 password를 가져올 수 있게 추가

--- src/main/java/Auction/service/repository/MemberRepository.java
+++ src/main/java/Auction/service/repository/MemberRepository.java
@@ -4,4 +4,5 @@ import Auction.service.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    Member findByUserId(String userId);
 }
