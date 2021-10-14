fe12356a8c64b6a2f01ba290721e7d2277be64a1
sohee0724 <ayrs02117a@gmail.com>
2021-10-14 11:57:24 +0900

    [#2] 회원가입, id 중복확인 테스트

--- src/test/java/Auction/service/service/MemberServiceTest.java
+++ src/test/java/Auction/service/service/MemberServiceTest.java
@@ -1,8 +1,8 @@
 package Auction.service.service;
 
 import Auction.service.domain.Member;
+import Auction.service.dto.MemberDto;
 import Auction.service.repository.MemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -17,19 +18,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberServiceTest {
 
     @Autowired
-    private MemberService memberService;
+    MemberService memberService;
+
     @Autowired
-    private MemberRepository memberRepository;
+    MemberRepository memberRepository;
 
     @Test
-    @DisplayName("멤버 생성 테스트")
-    public void createMember(){
-        Member member = new Member("testId", "testPassword");
-        memberService.createMember(member);
+    @DisplayName("회원가입 성공")
+    void registerMember(){
+        MemberDto memberDto = new MemberDto("ttttt", "pass", "ss", "0100000000");
 
-        Member findMember = memberRepository.getById(member.getId());
+        memberService.registerMember(memberDto);
 
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Member findMember = memberRepository.findByMemberId(memberDto.getId());
+        assertThat(findMember.getMemberId()).isEqualTo(memberDto.getId());
+    }
+
+    @Test
+    @DisplayName("id 중복 검사")
+    void checkMemberByMemberId() {
+        MemberDto memberDto1 = new MemberDto("test", "test1234", "name", "0100000000");
+        memberService.registerMember(memberDto1);
+
+        assertTrue(memberService.checkMemberByMemberId("test"));
+        assertFalse(memberService.checkMemberByMemberId("test2"));
     }
 
 }
