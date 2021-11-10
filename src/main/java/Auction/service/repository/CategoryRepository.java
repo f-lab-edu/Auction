7013b1cf966fd850262089568afc08ac7d176d4d
sohee0724 <ayrs02117a@gmail.com>
2021-11-10 13:57:29 +0900

    [#10] 카테고리, 상품, 상품이미지 repository 추가

---
+++ src/main/java/Auction/service/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package Auction.service.repository;
+
+import Auction.service.domain.product.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
