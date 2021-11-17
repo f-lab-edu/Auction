a3550c99a952e0ba6fc45f47fd95829e1784baed
sohee0724 <ayrs02117a@gmail.com>
2021-11-17 23:48:54 +0900

    [#14] S3Upload 수정

--- src/main/java/Auction/service/utils/S3Upload.java
+++ src/main/java/Auction/service/utils/S3Upload.java
@@ -1,5 +1,7 @@
 package Auction.service.utils;
 
+import Auction.service.domain.product.ProductImg;
+import Auction.service.exception.CustomMessageException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
@@ -11,26 +13,40 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Service
 @RequiredArgsConstructor
 public class S3Upload {
 
     private final AmazonS3Client amazonS3Client;
+    public static String PRODUCT_FOLDER = "product/";
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public String upload(MultipartFile file) throws IOException {
-        String fileName = UUID.randomUUID() + file.getOriginalFilename();
+    public String upload(MultipartFile file) {
 
-        ObjectMetadata objMeta = new ObjectMetadata();
-        byte[] bytes = IOUtils.toByteArray(file.getInputStream());
-        objMeta.setContentLength(bytes.length);
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
 
-        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), objMeta)
-                .withCannedAcl(CannedAccessControlList.PublicRead));
-        return amazonS3Client.getUrl(bucket, fileName).toString();
+        try {
+            ObjectMetadata objMeta = new ObjectMetadata();
+            byte[] bytes = IOUtils.toByteArray(file.getInputStream());
+            objMeta.setContentLength(bytes.length);
+
+            amazonS3Client.putObject(new PutObjectRequest(bucket, PRODUCT_FOLDER + fileName, file.getInputStream(), objMeta)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            throw new CustomMessageException(INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+
+        return fileName;
+    }
+
+    public void delete(String fileName) {
+        amazonS3Client.deleteObject(bucket, PRODUCT_FOLDER + fileName);
     }
 }
