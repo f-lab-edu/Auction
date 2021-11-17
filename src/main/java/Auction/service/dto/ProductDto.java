dd546d2e959dd9885a56c6c7e304f5199628014f
sohee0724 <ayrs02117a@gmail.com>
2021-11-17 23:46:11 +0900

    [#14] ProductDto 수정

--- src/main/java/Auction/service/dto/ProductDto.java
+++ src/main/java/Auction/service/dto/ProductDto.java
@@ -3,21 +3,22 @@ package Auction.service.dto;
 import Auction.service.domain.product.Product;
 import Auction.service.domain.product.ProductStatus;
 import Auction.service.domain.product.SaleType;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.validation.constraints.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductDto {
 
     @NotNull(message = "member_id가 없습니다")
     private Long member_id;
+
+    private Long product_id;
 
     @NotEmpty(message = "상품명을 입력해주세요")
     @Size(max = 30, message = "상품명은 최대 30자까지 입력 가능합니다")
@@ -38,40 +39,42 @@ public class ProductDto {
 
     private int fixPrice; //즉시 구매가
 
-    // 상품 판매 방법 : 고정가
-    public static Product toFixEntity(ProductDto productDto){
-        return Product.builder()
-                .name(productDto.getName())
-                .description(productDto.getDescription())
-                .fixPrice(productDto.getFixPrice())
-                .saleType(SaleType.valueOf(productDto.getSaleType()))
-                .status(ProductStatus.SALE)
-                .build();
-    }
+    public static Product toEntity(ProductDto productDto) {
 
-    // 상품 판매 방법 : 고정가 + 경매
-    public static Product toFixAndBiddingEntity(ProductDto productDto){
-        return Product.builder()
-                .name(productDto.getName())
-                .description(productDto.getDescription())
-                .fixPrice(productDto.getFixPrice())
-                .startPrice(productDto.getStartPrice())
-                .deadline(productDto.getDeadline())
-                .saleType(SaleType.valueOf(productDto.getSaleType()))
-                .status(ProductStatus.SALE)
-                .build();
-    }
+        String saleType = productDto.getSaleType();
 
-    // 상품 판매 방법 : 경매
-    public static Product toBiddingEntity(ProductDto productDto){
-        return Product.builder()
-                .name(productDto.getName())
-                .description(productDto.getDescription())
-                .startPrice(productDto.getStartPrice())
-                .deadline(productDto.getDeadline())
-                .saleType(SaleType.valueOf(productDto.getSaleType()))
-                .status(ProductStatus.SALE)
-                .build();
+        if (saleType.equals(SaleType.FIX_AND_BIDDING)) { // 상품 판매 방법 : 고정가 + 경매
+            return Product.builder()
+                    .name(productDto.getName())
+                    .description(productDto.getDescription())
+                    .fixPrice(productDto.getFixPrice())
+                    .startPrice(productDto.getStartPrice())
+                    .nowPrice(productDto.getStartPrice())
+                    .deadline(productDto.getDeadline())
+                    .saleType(SaleType.valueOf(saleType))
+                    .status(ProductStatus.SALE)
+                    .images(new ArrayList<>())
+                    .build();
+        } else if (saleType.equals(SaleType.FIX)) { // 상품 판매 방법 : 고정가
+            return Product.builder()
+                    .name(productDto.getName())
+                    .description(productDto.getDescription())
+                    .fixPrice(productDto.getFixPrice())
+                    .saleType(SaleType.valueOf(saleType))
+                    .status(ProductStatus.SALE)
+                    .images(new ArrayList<>())
+                    .build();
+        } else { // 상품 판매 방법 : 경매
+            return Product.builder()
+                    .name(productDto.getName())
+                    .description(productDto.getDescription())
+                    .startPrice(productDto.getStartPrice())
+                    .nowPrice(productDto.getStartPrice())
+                    .deadline(productDto.getDeadline())
+                    .saleType(SaleType.valueOf(saleType))
+                    .status(ProductStatus.SALE)
+                    .images(new ArrayList<>())
+                    .build();
+        }
     }
-
 }
