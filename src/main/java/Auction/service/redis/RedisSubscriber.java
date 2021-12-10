0284aaddbd3d585274d75b7a543ed19c2af93dc3
sohee0724 <ayrs02117a@gmail.com>
2021-12-11 01:53:25 +0900

    [#20] RedisSubscriber Component로 변경 및 기능 추가

--- src/main/java/Auction/service/redis/RedisSubscriber.java
+++ src/main/java/Auction/service/redis/RedisSubscriber.java
@@ -1,28 +1,50 @@
 package Auction.service.redis;
 
+import Auction.service.service.SseEmitterService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Component;
 
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
 @RequiredArgsConstructor
-@Slf4j
 public class RedisSubscriber implements MessageListener {
 
+    private final Map<String, String> channelLists = new ConcurrentHashMap();
+
     private final RedisMessageListenerContainer redisContainer;
+    private final RedisTemplate redisTemplate;
+    private final SseEmitterService sseEmitterService;
 
     public void createChannel(String channelName) {
-        redisContainer.addMessageListener(this, new ChannelTopic(channelName));
+        if (!channelLists.containsKey(channelName)) {
+            redisContainer.addMessageListener(this, new ChannelTopic(channelName));
+            channelLists.putIfAbsent(channelName, channelName);
+        }
     }
 
     public void removeChannel(String channelName) {
-        redisContainer.removeMessageListener(this, new ChannelTopic(channelName));
+        if (channelLists.containsKey(channelName)) {
+            redisContainer.removeMessageListener(this, new ChannelTopic(channelName));
+            // value = null 이면 해당 키 값 제거
+            channelLists.computeIfPresent(channelName,(key, value) -> null);
+        }
     }
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        log.info("message.toString() = " + message.toString());
+        String channelName = (String) redisTemplate.getStringSerializer().deserialize(message.getChannel());
+        String productPrice = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
+        int connectedSseEmittersSize  = sseEmitterService.notify(channelName, productPrice);
+        if (connectedSseEmittersSize == 0) {
+            removeChannel(channelName);
+        }
     }
 }
+
