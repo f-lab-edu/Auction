89ae81a9a6277fbf8dbd5ea14f0aaacc5bb59721
acecic82 <102938al@naver.com>
2021-12-14 15:05:12 +0900

    [#22] controller에 product를 Search하는 api 추가

--- src/main/java/Auction/service/controller/ProductSearchController.java
+++ src/main/java/Auction/service/controller/ProductSearchController.java
@@ -4,6 +4,7 @@ import Auction.service.dto.Result;
 import Auction.service.service.ProductSearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,5 +19,14 @@ public class ProductSearchController {
     public ResponseEntity<Result> getProductList(Pageable pageable) {
         return productSearchService.getList(pageable);
     }
+
+    @GetMapping("/search")
+    public ResponseEntity<Result> getProductListByName(@RequestParam(value = "name", required = false)String name,
+                                                       @RequestParam(value = "categoryId", required = false)Long categoryId,
+                                                       @PageableDefault(page = 0, size = 20) Pageable pageable){
+
+        return productSearchService.getListByName(name, categoryId, pageable);
+    }
+
 
 }
