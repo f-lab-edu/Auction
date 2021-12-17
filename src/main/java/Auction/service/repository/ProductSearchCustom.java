b6466cb79a16e23edf9f6ed8bfd5eed6f5530b81
acecic82 <102938al@naver.com>
2021-12-17 15:12:50 +0900

    [#22] productSearchCond 폴더 위치 변경하면서 import된 path 변경

--- src/main/java/Auction/service/repository/ProductSearchCustom.java
+++ src/main/java/Auction/service/repository/ProductSearchCustom.java
@@ -1,6 +1,6 @@
 package Auction.service.repository;
 
-import Auction.service.dto.ProductSearchCond;
+import Auction.service.condition.ProductSearchCond;
 import Auction.service.dto.ProductSearchDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
