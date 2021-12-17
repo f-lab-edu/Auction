fea3d4d8de22469263d7640b6aa88ba38d4911a1
acecic82 <102938al@naver.com>
2021-12-17 15:10:48 +0900

    [#22] controller에서 ResponseEntity를 만들어서 반환하도록 변경

--- src/main/java/Auction/service/controller/ProductSearchController.java
+++ src/main/java/Auction/service/controller/ProductSearchController.java
@@ -1,12 +1,16 @@
 package Auction.service.controller;
 
+import Auction.service.dto.ProductSearchDto;
 import Auction.service.dto.Result;
 import Auction.service.service.ProductSearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static Auction.service.utils.ResultCode.SUCCESS;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,18 +19,12 @@ public class ProductSearchController {
 
     private final ProductSearchService productSearchService;
 
-    @GetMapping
-    public ResponseEntity<Result> getProductList(Pageable pageable) {
-        return productSearchService.getList(pageable);
-    }
-
     @GetMapping("/search")
     public ResponseEntity<Result> getProductListByName(@RequestParam(value = "name", required = false)String name,
                                                        @RequestParam(value = "categoryId", required = false)Long categoryId,
                                                        @PageableDefault(page = 0, size = 20) Pageable pageable){
 
-        return productSearchService.getListByName(name, categoryId, pageable);
+        Page<ProductSearchDto> results = productSearchService.getListByNameAndCategory(name, categoryId, pageable);
+        return Result.toResponseEntity(SUCCESS,results);
     }
-
-
 }
--- src/main/java/Auction/service/service/ProductSearchService.java
+++ src/main/java/Auction/service/service/ProductSearchService.java
@@ -1,8 +1,7 @@
 package Auction.service.service;
 
-import Auction.service.Projection.ProductSearchProjection;
 import Auction.service.domain.product.ProductThumbnailState;
-import Auction.service.dto.ProductSearchCond;
+import Auction.service.condition.ProductSearchCond;
 import Auction.service.dto.ProductSearchDto;
 import Auction.service.dto.Result;
 import Auction.service.repository.ProductSearchRepository;
@@ -20,20 +19,14 @@ public class ProductSearchService {
 
     private final ProductSearchRepository productSearchRepository;
 
-    public ResponseEntity<Result> getList(Pageable pageable) {
-        ProductSearchCond condition = new ProductSearchCond(
-                null, null, ProductThumbnailState.THUMBNAIL);
+    public Page<ProductSearchDto> getListByNameAndCategory(String productName, Long categoryId, Pageable pageable){
+        ProductSearchCond condition = new ProductSearchCond();
+        condition.setProductName(productName);
+        condition.setCategoryId(categoryId);
         return getProductList(condition, pageable);
     }
 
-    public ResponseEntity<Result> getListByName(String productName, Long categoryId, Pageable pageable){
-        ProductSearchCond condition = new ProductSearchCond(productName, categoryId, ProductThumbnailState.THUMBNAIL);
-        return getProductList(condition, pageable);
-    }
-
-    private ResponseEntity<Result> getProductList(ProductSearchCond condition, Pageable pageable){
-        Page<ProductSearchDto> productSearchList = productSearchRepository
-                .findProductSearchList(condition, pageable);
-        return Result.toResponseEntity(SUCCESS, productSearchList);
+    private Page<ProductSearchDto> getProductList(ProductSearchCond condition, Pageable pageable){
+        return productSearchRepository.findProductSearchList(condition, pageable);
     }
 }
