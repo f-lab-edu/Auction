3c9adb8d28548b56ab396fb72a4039e5abd3e41d
acecic82 <102938al@naver.com>
2021-12-17 15:13:06 +0900

    [#22] 사용하지 않는 쿼리문 삭제

--- src/main/java/Auction/service/repository/ProductSearchRepository.java
+++ src/main/java/Auction/service/repository/ProductSearchRepository.java
@@ -11,13 +11,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductSearchRepository extends JpaRepository<Product, Long>, ProductSearchCustom {
 
-    @Query(nativeQuery = true,
-            value = "select p.product_id as id , p.name as productName, c.name as categoryName, p.sale_type as saleType, p.fix_price as fixPrice, p.now_price as nowPrice, pi.file_name as fileName " +
-                    "from product p left join category c on p.category_id=c.category_id  " +
-                    "left join product_img pi on pi.product_img_id = (select product_img_id from product_img pimg where pimg.product_id = p.product_id Limit 1) " +
-                    "where p.status='SALE' " +
-                    "order by p.reg_date desc",
-            countQuery = "select count(*) from product where status='SALE'")
-    Page<ProductSearchProjection> findProductSearchList(Pageable pageable);
-
 }
