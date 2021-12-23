59fa511f138f67809dcc94db4403135da25b3811
acecic82 <102938al@naver.com>
2021-12-24 03:32:33 +0900

    [#24] Service에 Redis annotation 추가와 Redis에 없을때 cache write하는 과정 추가

--- src/main/java/Auction/service/service/ProductSearchService.java
+++ src/main/java/Auction/service/service/ProductSearchService.java
@@ -1,29 +1,39 @@
 package Auction.service.service;
 
-import Auction.service.domain.product.ProductThumbnailState;
 import Auction.service.condition.ProductSearchCond;
 import Auction.service.dto.ProductSearchDto;
-import Auction.service.dto.Result;
+import Auction.service.cache.RedisCategoryCheck;
+import Auction.service.cache.RedisCategoryContainer;
+import Auction.service.cache.ProductSearchCacheRepository;
 import Auction.service.repository.ProductSearchRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import static Auction.service.utils.ResultCode.SUCCESS;
 
 @Service
 @RequiredArgsConstructor
 public class ProductSearchService {
 
     private final ProductSearchRepository productSearchRepository;
+    private final ProductSearchCacheRepository productSearchCacheRepository;
 
+    @RedisCategoryCheck
     public Page<ProductSearchDto> getListByNameAndCategory(String productName, Long categoryId, Pageable pageable){
         ProductSearchCond condition = new ProductSearchCond();
         condition.setProductName(productName);
         condition.setCategoryId(categoryId);
-        return getProductList(condition, pageable);
+        Page<ProductSearchDto> productList = getProductList(condition, pageable);
+
+        // cache write 시작
+        RedisCategoryContainer redisCategoryContainer = new RedisCategoryContainer();
+        redisCategoryContainer.setId(categoryId);
+        for (ProductSearchDto productSearchDto : productList) {
+            redisCategoryContainer.setProductItems(productSearchDto);
+        }
+        productSearchCacheRepository.save(redisCategoryContainer);
+
+        return productList;
     }
 
     private Page<ProductSearchDto> getProductList(ProductSearchCond condition, Pageable pageable){
